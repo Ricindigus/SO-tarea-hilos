61bbf5bbd13e3176b3f7d0001e37acc6b727cf0b
Ricardo Morales <ricindigus@gmail.com>
2015-11-26 22:29:35 -0500

    Agregue funcion busqueda secuencial

--- tareahilos.c
+++ tareahilos.c
@@ -4,13 +4,16 @@
 
 #define N 4096
 
-int fiRepetido(int iNumero,int iArreglo[],int tamArreglo);
+int fiRepetido(int iNumero,int iArreglo[], int tamArreglo);
+int fiBusquedaSecuencial(int iNumero,int iArreglo[], int tamArreglo);
 
 int main(int argc, char const *argv[])
 {
 	int iArreglo[N];
 	int iNumero;
 	int i;
+	
+	//Se llena el arreglo sin numero repetidos
 	for (i = 0; i < N; i++)
 	{
 		do{
@@ -19,6 +22,16 @@ int main(int argc, char const *argv[])
 		iArreglo[i] = iNumero;
 		printf("(%i)[%i]\n",i,iArreglo[i]);
 	}
+
+	//Se busca un elemento en el arreglo
+	int iNumeroABuscar;
+	printf("Ingrese numero a buscar: ");
+	scanf("%i",&iNumeroABuscar);
+	int p = fiBusquedaSecuencial(iNumeroABuscar,iArreglo,N);
+	if(p == -1)
+		printf("El elemento no esta en el arreglo\n");
+	else
+		printf("La posicion del numero buscado es: %i\n",p );
 }
 
 int fiRepetido(int iNumero,int iArreglo[],int tamArreglo)
@@ -37,4 +50,20 @@ int fiRepetido(int iNumero,int iArreglo[],int tamArreglo)
 	return encontrado;
 }
 
+int fiBusquedaSecuencial(int iNumero,int iArreglo[], int tamArreglo)
+{
+	int posicion = -1,i = 0;
+	int encontrado = 0;
+
+	while(!encontrado && i < tamArreglo)
+	{
+		if(iArreglo[i] == iNumero) 
+		{
+			encontrado = 1;
+			posicion = i;
+		}
+		i++;
+	}
+	return posicion;
+}
 
